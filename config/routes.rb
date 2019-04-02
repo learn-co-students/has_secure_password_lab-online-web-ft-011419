d8eb9b0b2ce68ac2775d42aed247cbe30e7e3f74
mikeygbd <mikey.soares@gmail.com>
2019-04-01 22:27:34 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  resources :users
+  get '/', to: 'sessions#index', as: 'root'
+  get '/login', to: 'sessions#new', as: 'login'
+
+  post '/sessions', to: 'sessions#create'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
