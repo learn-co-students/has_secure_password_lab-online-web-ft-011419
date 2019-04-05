8bc7223f56a363c4bc81cff320d514393a111b1b
gbs4ever <gbs4ever2@gmail.com>
2019-04-04 21:29:18 -0400

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,20 @@ class SessionsController < ApplicationController
   end
 
   def create
+   
+     @user = User.find_by(name: params[:user][:name])
+     #binding.pry
+   return head(:forbidden) unless @user.authenticate(params[:user][:password])
+   
+    session[:user_id] = @user.id
+   # binding.pry
+   redirect_to  welcome_path
   end
 
   def destroy
+  end
+   private
+  def user_params
+    params.require(:user).permit(:name,:password)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
-  get 'application/home'
+  get '/welcome' , to:'welcome#home'
 
-  get 'sessions/new'
+  
 
-  get 'sessions/create'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 
-  get 'sessions/destroy'
 
-  resources :users
+
+  resources :users ,only: [:new,:edit,:create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
